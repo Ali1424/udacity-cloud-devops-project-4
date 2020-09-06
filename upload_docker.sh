e3aab714d89e36306b14243ab6fede51adfa4197
Henok Hailemariam <henokhm2@gmail.com>
2020-09-06 09:58:18 +0300

    Task 4 - Upload the Docker Image

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=henokhm/cloud-devops-proj-4
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag 0407bc3e047a henokhm/cloud-devops-proj-4
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push henokhm/cloud-devops-proj-4
