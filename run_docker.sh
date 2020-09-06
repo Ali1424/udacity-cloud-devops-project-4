278f9dca87944ed1e0a326118db61c523ecc0497
Henok Hailemariam <henokhm2@gmail.com>
2020-09-06 03:14:12 +0300

    Task 2 - Complete run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build  --tag cloud-devops-proj-4 .
 
 # Step 2: 
 # List docker images
+docker container ls
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 cloud-devops-proj-4
